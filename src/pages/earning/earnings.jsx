9d03e972e0445d0631e4cb75e6e508a1812cf293
Paul Tanasă <21171252+pebly@users.noreply.github.com>
2021-09-19 09:50:46 +0300

    remove:  yarn

--- src/pages/earning/earnings.jsx
+++ src/pages/earning/earnings.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import classnames from 'classnames';
 import Mercedes from '../../assets/img/Mercedes-car.jpg';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Modal, ModalHeader, ModalBody, Collapse, Tooltip, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Popover, PopoverBody } from 'reactstrap';
+import CustomerReview from '../../assets/img/customer-review-img-1.png';
 
 export default function Earnings() {
   const [activeTab, setActiveTab] = useState('1');
+  const [showAddBankDetailsModal, setShowAddBankDetailsModal] = useState(false);
+
+  const modalCloseBtn = <button type="button" className="btn close p-0" onClick={() => {
+    setShowAddBankDetailsModal(false)
+  }}>
+    <span aria-hidden="true"><i className="fas fa-times-circle fa-lg"></i></span>
+  </button>;
+
+  function toggleAddBankDetailsModal(e) {
+    if (e) e.preventDefault();
+    setShowAddBankDetailsModal(!showAddBankDetailsModal);
+  };
 
   return (
     <>
@@ -64,7 +77,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p className="mb-0">On Hold</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline" onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -82,7 +95,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p className="mb-0">On Hold</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline" onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -100,7 +113,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p className="mb-0">On Hold</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline" onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -118,7 +131,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p className="mb-0">On Hold</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline"  onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -153,7 +166,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p>Processed</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline"  onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -171,7 +184,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p>Processed</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline"  onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -189,7 +202,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p>Processed</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline" onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -207,7 +220,7 @@ export default function Earnings() {
                         <div className="bookingVechile-detail">
                           <h2 className="text-dark-white">BMW</h2>
                           <p>Processed</p>
-                          <a href="#" className="text-underline">Booking Details</a>
+                          <a href="#" className="text-underline" onClick={toggleAddBankDetailsModal}>Booking Details</a>
                         </div>
                       </div>
                     </th>
@@ -221,6 +234,51 @@ export default function Earnings() {
             </div>
           </TabPane>
         </TabContent>
+        <Modal isOpen={showAddBankDetailsModal} toggle={toggleAddBankDetailsModal} className='bank-detail-popup'>
+          <ModalHeader toggle={toggleAddBankDetailsModal} close={modalCloseBtn}></ModalHeader>
+          <ModalBody style={{ padding: '0' }}>
+            <div className="add-member-header text-center mb-5">
+              <h2 style={{ fontWeight: '600' }}>Booking Details</h2>
+            </div>
+            <form>
+              <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', padding: '0 30px 10px 30px' }}>
+                <img src={Mercedes} style={{ width: '120px', borderRadius: '10px' }} />
+                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
+                  <h5 style={{ fontWeight: '600' }}>Mercedes Benz 2018</h5>
+                  <p style={{ color: '#f67810', marginBottom: '10px', fontWeight: '500' }}>HL8 HXM</p>
+                  <p style={{ display: 'flex', alignItems: 'center' }}>
+                    Booked by
+                    <img src={CustomerReview} alt="rated-person" style={{ width: '30px', marginLeft: '10px', marginRight: '5px' }} />
+                    Thomas H.
+                  </p>
+                </div>
+              </div>
+              <hr />
+              <p style={{ padding: '0 30px 10px 30px' }}>
+                Checked in: <span style={{ color: '#f67810', fontWeight: '500' }}>10 April 2021 - 06:30AM</span>
+              </p>
+              <p style={{ padding: '0 30px 10px 30px' }}>
+                Return Date: <span style={{ color: '#f67810', fontWeight: '500' }}>14 April 2021 - 07:00PM</span>
+              </p>
+              <p style={{ padding: '0 30px 30px 30px' }}>
+                Booked for: <span style={{ color: '#f67810', fontWeight: '500' }}>5 days</span>
+              </p>
+
+              <div style={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column', width: '200px', marginLeft: 'auto' }}>
+                <p style={{ padding: '0 30px 0 30px' }}>
+                  Amount: <span style={{ color: '#f67810', fontWeight: '500', width: '70px', display: 'inline-block' }}>£25.99</span>
+                </p>
+                <p style={{ padding: '0 30px 0 30px' }}>
+                  Fees: <span style={{ color: '#f67810', fontWeight: '500', width: '70px', display: 'inline-block' }}>-£7</span>
+                </p>
+                <hr style={{width: '200px', marginTop: '5px', marginBottom: '5px'}} />
+                <p style={{ padding: '5px 30px 15px 30px', fontWeight: '600' }}>
+                  Total: <span style={{ color: '#f67810', fontWeight: '600', width: '70px', display: 'inline-block' }}>£18.99</span>
+                </p>
+              </div>
+            </form>
+          </ModalBody>
+        </Modal>
       </div>
     </>
   );
